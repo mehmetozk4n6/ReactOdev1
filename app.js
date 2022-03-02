6586256670f19ac5f405b94a2302f7d4d010768b
Mehmet Özkan <81770287+mehmetozk4n6@users.noreply.github.com>
2022-03-02 19:07:57 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+import axios from "axios";
+
+export const getData = async (user_id) => {
+    const {data:User} = await axios("https://jsonplaceholder.typicode.com/users/"+user_id)
+    const {data:Posts} = await axios("https://jsonplaceholder.typicode.com/posts?userId="+user_id)
+    
+    return {User,Posts};
+}
